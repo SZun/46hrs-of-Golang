a0ec983491796b543e62dd35d3cd9f6e5583f341
Zun <zunsamuel97@gmail.com>
2019-01-22 22:05:02 -0500

    hoe5

--- section-5/main.go
+++ section-5/main.go
@@ -2,11 +2,17 @@ package main
 
 import "fmt"
 
+type myOwnType int
+
+var x myOwnType
+var y int
+
 func main() {
-	type myOwnType int
-	var x myOwnType
 	fmt.Println(x)
 	fmt.Printf("%T\n", x)
 	x = 42
 	fmt.Println(x)
+	y := int(x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
 }
