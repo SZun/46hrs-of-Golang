dc4d8f122fd0ce48fca2c4e62546179cf30a21f4
Zun <zunsamuel97@gmail.com>
2019-01-22 20:50:06 -0500

    packages

--- section-4/main.go
+++ section-4/main.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
-func anything() {
+func main() {
 	x := 42
 	fmt.Println(x)
 	x = 99
 	fmt.Println(x)
-	y := 100 + 24
+	y := 100 + 70
 	fmt.Println(y)
+	z := "Bond, James"
+	fmt.Println(z)
 }
