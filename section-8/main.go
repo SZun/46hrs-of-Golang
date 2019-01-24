be77958e78cbdbed739bda4abacded34ed388365
Zun <zunsamuel97@gmail.com>
2019-01-23 20:38:57 -0500

    else

--- section-8/main.go
+++ section-8/main.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main() {
 	x := 42
-	if x == 2 {
-		fmt.Println("001")
+	if x == 40 {
+		fmt.Println("40")
+	} else if x == 41 {
+		fmt.Println("!40")
+	} else {
+		fmt.Println("42")
 	}
-	fmt.Println(x)
 }
