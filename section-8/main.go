736e56c1e69d1986e17cb0c874eae4ca29b61564
Zun <zunsamuel97@gmail.com>
2019-01-23 20:54:46 -0500

    logical operators

--- section-8/main.go
+++ section-8/main.go
@@ -3,17 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	switch {
-	case false:
-		fmt.Println("false")
-	case 2 == 4:
-		fmt.Println("2 == 4")
-	case 3 == 3:
-		fmt.Println("3 == 3")
-		fallthrough
-	case 7 == 9:
-		fmt.Println("7 == 9")
-	case 4 == 4:
-		fmt.Println("4 == 4")
-	}
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
