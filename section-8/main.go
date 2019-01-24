1edbcca4830495f0f4f32e003b6ec83b6eeac560
Zun <zunsamuel97@gmail.com>
2019-01-23 20:17:21 -0500

    break

--- section-8/main.go
+++ section-8/main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i <= 10; i++ {
-		fmt.Printf("The outer loop: %d\n", i)
-		for j := 0; j < 3; j++ {
-			fmt.Printf("The inner loop: %d\n", j)
+	x := 1
+	for {
+		if x > 9 {
+			break
 		}
+		fmt.Println(x)
+		x++
 	}
+	fmt.Println("done.")
 }
