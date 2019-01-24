952e9dbb99f375bfd963ba0b1560983701628f2e
Zun <zunsamuel97@gmail.com>
2019-01-23 19:22:14 -0500

    iota

--- section-6/main.go
+++ section-6/main.go
@@ -3,16 +3,22 @@ package main
 import "fmt"
 
 const (
-	a int     = 42
-	b float64 = 42.78
-	c string  = "James Bond"
+	a = iota
+	b
+	c
+)
+
+const (
+	d = iota
+	e
+	f
 )
 
 func main() {
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
-	fmt.Printf("%T\n", a)
-	fmt.Printf("%T\n", b)
-	fmt.Printf("%T\n", c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
 }
