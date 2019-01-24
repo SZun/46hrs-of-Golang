df13b249c2a4ceee2497e319547215e23a4ab8fc
Zun <zunsamuel97@gmail.com>
2019-01-23 20:36:39 -0500

    if

--- section-8/main.go
+++ section-8/main.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	for i := 33; i <= 122; i++ {
-		fmt.Printf("%v\t%#X\t%#U\n", i, i, i)
+	x := 42
+	if x == 2 {
+		fmt.Println("001")
 	}
+	fmt.Println(x)
 }
