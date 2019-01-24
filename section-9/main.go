c5f53153c2897591dcea09a2b035bc8d23416c92
Zun <zunsamuel97@gmail.com>
2019-01-23 21:45:20 -0500

    hoe3-5

--- section-9/main.go
+++ section-9/main.go
@@ -3,14 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	bd := 1990
-
-	for {
-		if bd > 2019 {
-			break
-		}
-		fmt.Println(bd)
-		bd++
-
+	for i := 0; i < 100; i++ {
+		fmt.Println(i % 4)
 	}
 }
