61705ca9a84f30e2354146ffe467d7cdbead3bde
Zun <zunsamuel97@gmail.com>
2019-01-23 20:46:53 -0500

    switch

--- section-8/main.go
+++ section-8/main.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 100; i++ {
-		if i%3 == 0 {
-			fmt.Println(i)
-		}
+	switch {
+	case false:
+		fmt.Println("false")
+	case 2 == 4:
+		fmt.Println("2 == 4")
+	case 3 == 3:
+		fmt.Println("3 == 3")
+		fallthrough
+	case 7 == 9:
+		fmt.Println("7 == 9")
+	case 4 == 4:
+		fmt.Println("4 == 4")
 	}
 }
