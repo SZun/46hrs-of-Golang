ea0b6c305e81b398e8512da369c3c224e2818065
Zun <zunsamuel97@gmail.com>
2019-01-23 21:54:37 -0500

    hoe3-8

--- section-9/main.go
+++ section-9/main.go
@@ -3,12 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var x string
-	if x == "Moneypenny" {
-		fmt.Println(x)
-	} else if x == "James Bond" {
-		fmt.Println(x)
-	} else {
-		fmt.Println("neither")
+	switch {
+	case false:
+		fmt.Println("false")
+	case true:
+		fmt.Println("true")
 	}
 }
