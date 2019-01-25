b7caa5f3aca2a903321a87197455feee56526ae9
Zun <zunsamuel97@gmail.com>
2019-01-24 20:31:01 -0500

    map looping

--- section-10/main.go
+++ section-10/main.go
@@ -15,7 +15,15 @@ func main() {
 	v, ok := m["8"]
 	fmt.Println(v)
 	fmt.Println(ok)
+	m["6"] = 6
 	if v, ok := m["3"]; ok {
 		fmt.Println(v)
+	}
+	for i, v := range m {
+		fmt.Println(i, v)
+	}
+	xi := []int{1, 2, 3, 4, 5}
+	for i, v := range xi {
+		fmt.Println(i, v)
 	}
 }
