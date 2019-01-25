44b1469baf6a8023a5546d2b16fd548d37ef17e3
Zun <zunsamuel97@gmail.com>
2019-01-24 21:52:55 -0500

    hoe5-2

--- section-13/main.go
+++ section-13/main.go
@@ -19,6 +19,21 @@ func main() {
 		last:  "zun",
 		ficf:  []string{"a", "b", "c"},
 	}
+
+	m := map[string]person{
+		p1.last: p1,
+		p2.last: p2,
+	}
+
+	fmt.Println(m)
+
+	for k, v := range m {
+		fmt.Println(k, v)
+		for j, val := range v.ficf {
+			fmt.Println(j, val)
+		}
+	}
+
 	fmt.Println(p1.first, p1.last)
 	for _, v := range p1.ficf {
 		fmt.Println(v)
