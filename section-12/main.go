281d14ae5c2c952a2858d5349faded78785f9c28
Zun <zunsamuel97@gmail.com>
2019-01-24 21:31:34 -0500

    anon-struct

--- section-12/main.go
+++ section-12/main.go
@@ -2,33 +2,16 @@ package main
 
 import "fmt"
 
-type person struct {
-	first string
-	last  string
-	age   int
-}
-
-type secretAgent struct {
-	person
-	ltk bool
-}
-
 func main() {
-	sa1 := secretAgent{
-		person: person{
-			first: "Samuel",
-			last:  "Zun",
-			age:   21,
-		},
-		ltk: false,
-	}
-	p2 := person{
+	p2 := struct {
+		first string
+		last  string
+		age   int
+	}{
 		first: "Rimma",
 		last:  "Zun",
 		age:   21,
 	}
-	fmt.Println(sa1)
 	fmt.Println(p2)
-	fmt.Println(sa1.first, sa1.last, sa1.age, sa1.ltk)
 	fmt.Println(p2.first, p2.last, p2.age)
 }
