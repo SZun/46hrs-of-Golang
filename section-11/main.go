952202d3d52eae1505e92451b368174859451fb0
Zun <zunsamuel97@gmail.com>
2019-01-24 20:54:56 -0500

    hoe4-6

--- section-11/main.go
+++ section-11/main.go
@@ -3,7 +3,58 @@ package main
 import "fmt"
 
 func main() {
-	x := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	x = append(x[:3], x[6:]...)
-	fmt.Println(x)
+	s := make([]string, 50, 50)
+	s = []string{"AK - Alaska",
+		"AL - Alabama",
+		"AR - Arkansas",
+		"AZ - Arizona",
+		"CA - California",
+		"CO - Colorado",
+		"CT - Connecticut",
+		"DE - Delaware",
+		"FL - Florida",
+		"GA - Georgia",
+		"HI - Hawaii",
+		"IA - Iowa",
+		"ID - Idaho",
+		"IL - Illinois",
+		"IN - Indiana",
+		"KS - Kansas",
+		"KY - Kentucky",
+		"LA - Louisiana",
+		"MA - Massachusetts",
+		"MD - Maryland",
+		"ME - Maine",
+		"MI - Michigan",
+		"MN - Minnesota",
+		"MO - Missouri",
+		"MS - Mississippi",
+		"MT - Montana",
+		"NC - North Carolina",
+		"ND - North Dakota",
+		"NE - Nebraska",
+		"NH - New Hampshire",
+		"NJ - New Jersey",
+		"NM - New Mexico",
+		"NV - Nevada",
+		"NY - New York",
+		"OH - Ohio",
+		"OK - Oklahoma",
+		"OR - Oregon",
+		"PA - Pennsylvania",
+		"RI - Rhode Island",
+		"SC - South Carolina",
+		"SD - South Dakota",
+		"TN - Tennessee",
+		"TX - Texas",
+		"UT - Utah",
+		"VA - Virginia",
+		"VT - Vermont",
+		"WA - Washington",
+		"WI - Wisconsin",
+		"WV - West Virginia",
+		"WY - Wyoming"}
+	fmt.Println(s)
+	fmt.Println(len(s))
+	fmt.Println(cap(s))
 }
