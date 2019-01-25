74ae47486a65885ec693f5bd969b2d89d43130f3
Zun <zunsamuel97@gmail.com>
2019-01-24 19:18:17 -0500

    slicing a slice

--- section-10/main.go
+++ section-10/main.go
@@ -4,14 +4,11 @@ import "fmt"
 
 func main() {
 	x := []int{4, 5, 7, 8, 42}
-	fmt.Println(len(x))
-	fmt.Println(x)
-	fmt.Println(x[0])
 	fmt.Println(x[1])
-	fmt.Println(x[2])
-	fmt.Println(x[3])
-	fmt.Println(x[4])
-	for i, v := range x {
-		fmt.Println(i, v)
+	fmt.Println(x)
+	fmt.Println(x[1:])
+	fmt.Println(x[1:3])
+	for i := 0; i <= 4; i++ {
+		fmt.Println(i, x[i])
 	}
 }
