9bd56e828fe7873c25b15d800aca5a080ced61ef
Zun <zunsamuel97@gmail.com>
2019-01-24 20:50:30 -0500

    hoe4-5

--- section-11/main.go
+++ section-11/main.go
@@ -4,11 +4,6 @@ import "fmt"
 
 func main() {
 	x := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	x = append(x, 52)
-	fmt.Println(x)
-	x = append(x, 53, 54, 55)
-	fmt.Println(x)
-	y := []int{11, 12, 13}
-	x = append(x, y...)
+	x = append(x[:3], x[6:]...)
 	fmt.Println(x)
 }
