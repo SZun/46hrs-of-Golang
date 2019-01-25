3d913af30e3a3b84cdfd09c33a24b2a6acfc7726
Zun <zunsamuel97@gmail.com>
2019-01-24 21:13:16 -0500

    hoe4-8

--- section-11/main.go
+++ section-11/main.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	xs1 := []string{"AK - Alaska", "AL - Alabama", "AR - Arkansas"}
-	xs2 := []string{"AK - Alaska", "AL - Alabama", "AR - Arkansas"}
-	fmt.Println(xs1)
-	fmt.Println(xs2)
-	xxs := [][]string{xs1, xs2}
-	fmt.Println(xxs)
-	for _, xs := range xxs {
-		for _, v := range xs {
-			fmt.Println(v)
+	m := map[string][]string{
+		"rimma": []string{`1`, `2`, `3`},
+		"jeff":  []string{`1`, `2`, `3`},
+		"sam":   []string{`1`, `2`, `3`},
+	}
+	for i, v := range m {
+		fmt.Println(i)
+		for j, k := range v {
+			fmt.Println(j, k)
 		}
 	}
 }
