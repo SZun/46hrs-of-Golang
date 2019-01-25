293ed462a207b1d3235b38e320db3d8bc7d9dde5
Zun <zunsamuel97@gmail.com>
2019-01-24 21:22:11 -0500

    struct-2

--- section-12/main.go
+++ section-12/main.go
@@ -5,19 +5,22 @@ import "fmt"
 type person struct {
 	first string
 	last  string
+	age   int
 }
 
 func main() {
 	p1 := person{
 		first: "Samuel",
 		last:  "Zun",
+		age:   21,
 	}
 	p2 := person{
 		first: "Rimma",
 		last:  "Zun",
+		age:   21,
 	}
 	fmt.Println(p1)
 	fmt.Println(p2)
-	fmt.Println(p1.first, p1.last)
-	fmt.Println(p2.first, p2.last)
+	fmt.Println(p1.first, p1.last, p1.age)
+	fmt.Println(p2.first, p2.last, p2.age)
 }
