aa14701eae367857209ae2969e4622014a797a45
Zun <zunsamuel97@gmail.com>
2019-01-24 21:56:39 -0500

    hoe5-3

--- section-13/main.go
+++ section-13/main.go
@@ -2,44 +2,38 @@ package main
 
 import "fmt"
 
-type person struct {
-	first string
-	last  string
-	ficf  []string
+type vehicle struct {
+	doors int
+	color string
+}
+
+type truck struct {
+	vehicle
+	fourWheel bool
+}
+
+type sedan struct {
+	vehicle
+	luxury bool
 }
 
 func main() {
-	p1 := person{
-		first: "sam",
-		last:  "zun",
-		ficf:  []string{"a", "b", "c"},
+	t := truck{
+		vehicle: vehicle{
+			doors: 2,
+			color: "red",
+		},
+		fourWheel: true,
 	}
-	p2 := person{
-		first: "jeff",
-		last:  "zun",
-		ficf:  []string{"a", "b", "c"},
+	fmt.Println(t)
+	s := sedan{
+		vehicle: vehicle{
+			doors: 4,
+			color: "white",
+		},
+		luxury: false,
 	}
-
-	m := map[string]person{
-		p1.last: p1,
-		p2.last: p2,
-	}
-
-	fmt.Println(m)
-
-	for k, v := range m {
-		fmt.Println(k, v)
-		for j, val := range v.ficf {
-			fmt.Println(j, val)
-		}
-	}
-
-	fmt.Println(p1.first, p1.last)
-	for _, v := range p1.ficf {
-		fmt.Println(v)
-	}
-	fmt.Println(p2.first, p2.last)
-	for _, v := range p2.ficf {
-		fmt.Println(v)
-	}
+	fmt.Println(s)
+	fmt.Println(s.doors)
+	fmt.Println(t.doors)
 }
