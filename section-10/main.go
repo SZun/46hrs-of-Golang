e984027e6c13e34c1b13e2b9f0e0075aa332d489
Zun <zunsamuel97@gmail.com>
2019-01-24 19:24:52 -0500

    delete slice

--- section-10/main.go
+++ section-10/main.go
@@ -7,7 +7,11 @@ func main() {
 	fmt.Println(x)
 	x = append(x, 77, 88, 99, 1014)
 	fmt.Println(x)
+
 	y := []int{234, 456, 678, 987}
 	x = append(x, y...)
+	fmt.Println(x)
+
+	x = append(x[:2], x[4:]...)
 	fmt.Println(x)
 }
