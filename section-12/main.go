36c6730e0e8472f89860457d26847201c5141355
Zun <zunsamuel97@gmail.com>
2019-01-24 21:26:09 -0500

    embedded struct

--- section-12/main.go
+++ section-12/main.go
@@ -8,19 +8,27 @@ type person struct {
 	age   int
 }
 
+type secretAgent struct {
+	person
+	ltk bool
+}
+
 func main() {
-	p1 := person{
-		first: "Samuel",
-		last:  "Zun",
-		age:   21,
+	sa1 := secretAgent{
+		person: person{
+			first: "Samuel",
+			last:  "Zun",
+			age:   21,
+		},
+		ltk: false,
 	}
 	p2 := person{
 		first: "Rimma",
 		last:  "Zun",
 		age:   21,
 	}
-	fmt.Println(p1)
+	fmt.Println(sa1)
 	fmt.Println(p2)
-	fmt.Println(p1.first, p1.last, p1.age)
+	fmt.Println(sa1.first, sa1.last, sa1.age, sa1.ltk)
 	fmt.Println(p2.first, p2.last, p2.age)
 }
