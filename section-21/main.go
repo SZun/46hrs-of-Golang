137e27598126071fd9163eb88bfec04d474b2449
Zun <zunsamuel97@gmail.com>
2019-01-27 14:10:36 -0500

    hoe9-4

--- section-21/main.go
+++ section-21/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 	"sync"
 )
 
@@ -11,13 +10,15 @@ func main() {
 	incrementor := 0
 	gs := 100
 	wg.Add(gs)
+	var m sync.Mutex
 	for i := 0; i < gs; i++ {
 		go func() {
+			m.Lock()
 			v := incrementor
-			runtime.Gosched()
 			v++
 			incrementor = v
 			fmt.Println(incrementor)
+			m.Unlock()
 			wg.Done()
 		}()
 	}
