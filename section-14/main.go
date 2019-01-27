ee5e4de788fb9eb88fb8a87b7945c9c00769338e
Zun <zunsamuel97@gmail.com>
2019-01-26 21:13:40 -0500

    variadic parameters

--- section-14/main.go
+++ section-14/main.go
@@ -3,29 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	foo()
-	bar("Sam")
-	s1 := woo("MP")
-	fmt.Println(s1)
-	x, y := mouse("sam", "zun")
+	foo(2, 3, 4, 5, 6, 7, 8, 9)
+}
+
+func foo(x ...int) {
 	fmt.Println(x)
-	fmt.Println(y)
-}
+	fmt.Printf("%T\n", x)
 
-func foo() {
-	fmt.Println("foo")
-}
-
-func bar(s string) {
-	fmt.Println("Hello, ", s)
-}
-
-func woo(s string) string {
-	return fmt.Sprint("Hello, from woo " + s)
-}
-
-func mouse(fn string, ln string) (string, bool) {
-	a := fmt.Sprint(fn, ln, " says hello")
-	b := true
-	return a, b
+	sum := 0
+	for i, v := range x {
+		sum += v
+		fmt.Println(i, v, sum)
+	}
+	fmt.Println(sum)
 }
