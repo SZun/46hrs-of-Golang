122210c8020c468872d269e85d74edc5fc53e55d
Zun <zunsamuel97@gmail.com>
2019-01-27 18:29:26 -0500

    comma ok idiom

--- section-22/main.go
+++ section-22/main.go
@@ -3,36 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	eve := make(chan int)
-	odd := make(chan int)
-	quit := make(chan int)
-	go send(eve, odd, quit)
-	recieve(eve, odd, quit)
-}
-
-func recieve(e, o, q <-chan int) {
-	for {
-		select {
-		case v := <-e:
-			fmt.Println("from the even channel:\t", v)
-		case v := <-o:
-			fmt.Println("from the odd channel:\t", v)
-		case v := <-q:
-			fmt.Println("from the quit channel:\t", v)
-			return
-		}
-	}
-}
-
-func send(e, o, q chan<- int) {
-	for i := 0; i < 100; i++ {
-		if i%2 == 0 {
-			e <- i
-		} else {
-			o <- i
-		}
-	}
-	// close(e)
-	// close(o)
-	q <- 0
+	c := make(chan int)
+	go func() {
+		c <- 42
+		close(c)
+	}()
+	v, ok := <-c
+	fmt.Println(v, ok)
+	v, ok = <-c
+	fmt.Println(v, ok)
 }
