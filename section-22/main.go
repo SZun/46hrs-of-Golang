c697af7c3780392ae3bc4437f02116beefba2292
Zun <zunsamuel97@gmail.com>
2019-01-27 18:20:37 -0500

    select channels

--- section-22/main.go
+++ section-22/main.go
@@ -3,17 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	c := make(chan int)
-	// send
-	go func() {
-		for i := 0; i < 100; i++ {
-			c <- i
+	eve := make(chan int)
+	odd := make(chan int)
+	quit := make(chan int)
+	go send(eve, odd, quit)
+	recieve(eve, odd, quit)
+}
+
+func recieve(e, o, q <-chan int) {
+	for {
+		select {
+		case v := <-e:
+			fmt.Println("from the even channel:\t", v)
+		case v := <-o:
+			fmt.Println("from the odd channel:\t", v)
+		case v := <-q:
+			fmt.Println("from the quit channel:\t", v)
+			return
 		}
-		close(c)
-	}()
-	// recieve
-	for v := range c {
-		fmt.Println(v)
 	}
-	fmt.Println("done")
+}
+
+func send(e, o, q chan<- int) {
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			e <- i
+		} else {
+			o <- i
+		}
+	}
+	// close(e)
+	// close(o)
+	q <- 0
 }
