7b5767bacfb02911c26a91db84974cab5f82cf79
Zun <zunsamuel97@gmail.com>
2019-01-27 11:45:46 -0500

    writer inteface

--- section-18/main.go
+++ section-18/main.go
@@ -1,29 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io"
+	"os"
 )
 
-type person struct {
-	First string `json:"First"`
-	Last  string `json:"Last"`
-	Age   int    `json:"Age"`
-}
-
 func main() {
-	s := `[{"First":"James","Last":"Bond","Age":32},{"First":"Miss","Last":"Moneypenny","Age":27}]`
-	bs := []byte(s)
-	fmt.Printf("%T\n", s)
-	fmt.Printf("%T\n", bs)
-	people := []person{}
-	err := json.Unmarshal(bs, &people)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("all of the data", people)
-	for i, v := range people {
-		fmt.Println("person number", i)
-		fmt.Println(v.First, v.Last, v.Age)
-	}
+	fmt.Fprintln(os.Stdout, "Hello playground")
+	io.WriteString(os.Stdout, "Hello playground")
+
 }
