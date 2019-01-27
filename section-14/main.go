63b56d20f2348527bd28ef1d0f2a03c2beb5f53c
Zun <zunsamuel97@gmail.com>
2019-01-26 21:31:27 -0500

    defer

--- section-14/main.go
+++ section-14/main.go
@@ -3,21 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	xi := []int{2, 3, 4, 5, 6, 7, 8, 9}
-	x := foo(xi...)
-	fmt.Println(x)
-	foo()
+	defer foo()
+	bar()
 }
 
-func foo(x ...int) int {
-	fmt.Println(x)
-	fmt.Printf("%T\n", x)
+func foo() {
+	fmt.Println("foo")
+}
 
-	sum := 0
-	for i, v := range x {
-		sum += v
-		fmt.Println(i, v, sum)
-	}
-	fmt.Println(sum)
-	return sum
+func bar() {
+	fmt.Println("bar")
 }
