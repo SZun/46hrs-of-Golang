4ad81646d753b84a7c75a8f5222ee2a49913880a
Zun <zunsamuel97@gmail.com>
2019-01-27 09:43:28 -0500

    callback

--- section-14/main.go
+++ section-14/main.go
@@ -3,21 +3,40 @@ package main
 import "fmt"
 
 func main() {
-	s1 := foo()
-	fmt.Println(s1)
-	s2 := bar()
-	fmt.Printf("%T\n", s2)
-	i := s2()
-	fmt.Println(i)
+	ii := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	s := sum(ii...)
+	fmt.Println("all numbers", s)
+	s2 := even(sum, ii...)
+	fmt.Println("even numbers", s2)
+	s3 := odd(sum, ii...)
+	fmt.Println("odd numbers", s3)
 }
 
-func bar() func() int {
-	return func() int {
-		return 451
+func sum(xi ...int) int {
+	fmt.Printf("%T\n", xi)
+	total := 0
+	for _, v := range xi {
+		total += v
 	}
+	return total
 }
 
-func foo() string {
-	s := "Hello World"
-	return s
+func even(f func(xi ...int) int, vi ...int) int {
+	var yi []int
+	for _, v := range vi {
+		if v%2 == 0 {
+			yi = append(yi, v)
+		}
+	}
+	return f(yi...)
+}
+
+func odd(f func(xi ...int) int, vi ...int) int {
+	var yi []int
+	for _, v := range vi {
+		if v%2 != 0 {
+			yi = append(yi, v)
+		}
+	}
+	return f(yi...)
 }
