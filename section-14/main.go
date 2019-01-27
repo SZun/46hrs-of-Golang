1cd1ea50f16f4fd35149ec37699cbad19383f77a
Zun <zunsamuel97@gmail.com>
2019-01-26 22:12:06 -0500

    interfaces

--- section-14/main.go
+++ section-14/main.go
@@ -12,8 +12,29 @@ type secretAgent struct {
 	ltk bool
 }
 
+type human interface {
+	speak()
+}
+
+type hotdog int
+
 func (s secretAgent) speak() {
 	fmt.Println(s.last+",", s.first, s.last)
+}
+
+func (p person) speak() {
+	fmt.Println(p.last+",", p.first, p.last)
+}
+
+func bar(h human) {
+	switch h.(type) {
+	case person:
+		fmt.Println("person: ", h.(person).last)
+	case secretAgent:
+		fmt.Println("secretAgent: ", h)
+	default:
+		return
+	}
 }
 
 func main() {
@@ -31,7 +52,26 @@ func main() {
 		},
 		ltk: true,
 	}
+
+	p1 := person{
+		first: "Sam",
+		last:  "Zun",
+	}
+
+	fmt.Println(p1)
 	fmt.Println(sa1)
 	sa1.speak()
 	sa2.speak()
+	bar(sa1)
+	bar(p1)
+	p1.speak()
+
+	// conversion
+	var x hotdog = 42
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	var y int
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
 }
