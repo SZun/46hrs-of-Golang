bdc1122d59f8fd635cf0787abc53f51d40d3add8
Zun <zunsamuel97@gmail.com>
2019-01-27 11:17:15 -0500

    json unmarshal

--- section-18/main.go
+++ section-18/main.go
@@ -6,28 +6,24 @@ import (
 )
 
 type person struct {
-	First string
-	Last  string
-	Age   int
+	First string `json:"First"`
+	Last  string `json:"Last"`
+	Age   int    `json:"Age"`
 }
 
 func main() {
-	p1 := person{
-		First: "James",
-		Last:  "Bond",
-		Age:   32,
-	}
-	p2 := person{
-		First: "Miss",
-		Last:  "Moneypenny",
-		Age:   27,
-	}
-
-	people := []person{p1, p2}
-	fmt.Println(people)
-	bs, err := json.Marshal(people)
+	s := `[{"First":"James","Last":"Bond","Age":32},{"First":"Miss","Last":"Moneypenny","Age":27}]`
+	bs := []byte(s)
+	fmt.Printf("%T\n", s)
+	fmt.Printf("%T\n", bs)
+	people := []person{}
+	err := json.Unmarshal(bs, &people)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(bs))
+	fmt.Println("all of the data", people)
+	for i, v := range people {
+		fmt.Println("person number", i)
+		fmt.Println(v.First, v.Last, v.Age)
+	}
 }
