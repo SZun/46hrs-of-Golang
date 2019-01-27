53a17b525df456d11043197062b9dda19a4923ed
Zun <zunsamuel97@gmail.com>
2019-01-27 10:44:12 -0500

    pointers-2

--- section-16/main.go
+++ section-16/main.go
@@ -3,14 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	a := 42
-	fmt.Println(a)
-	fmt.Println(&a)
-	fmt.Printf("%T\n", a)
-	fmt.Printf("%T\n", &a)
-	b := &a
-	fmt.Println(b)
-	fmt.Println(*b)
-	*b = 43
-	fmt.Println(a)
+	x := 0
+	foo(&x)
+	fmt.Println(x)
+}
+
+func foo(y *int) {
+	fmt.Println(*y)
+	*y = 43
+	fmt.Println(*y)
 }
