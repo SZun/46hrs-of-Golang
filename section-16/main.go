8b52a7b2ab782583167b29ac4bed1fc3768b69cb
Zun <zunsamuel97@gmail.com>
2019-01-27 10:49:15 -0500

    method set

--- section-16/main.go
+++ section-16/main.go
@@ -1,15 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main() {
-	x := 0
-	foo(&x)
-	fmt.Println(x)
+type square struct {
+	length float64
 }
 
-func foo(y *int) {
-	fmt.Println(*y)
-	*y = 43
-	fmt.Println(*y)
+type circle struct {
+	radius float64
+}
+
+type shape interface {
+	area() float64
+}
+
+func main() {
+	s := square{
+		length: 4.454635,
+	}
+	c := circle{
+		radius: 4.23456,
+	}
+	info(s)
+	fmt.Printf("%T\n", &c)
+	info(&c)
+}
+
+func info(s shape) {
+	fmt.Println(s.area())
+}
+
+func (s square) area() float64 {
+	return s.length * s.length
+}
+func (c *circle) area() float64 {
+	return math.Pi * c.radius * c.radius
 }
