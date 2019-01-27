b3b989220a282b1f84ceaa01432689725d654527
Zun <zunsamuel97@gmail.com>
2019-01-27 09:59:33 -0500

    hoe6-3

--- section-15/main.go
+++ section-15/main.go
@@ -3,15 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	si := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	f1 := foo(si...)
-	fmt.Println(f1)
+	defer foo()
+	bar()
 }
 
-func foo(i ...int) int {
-	total := 0
-	for _, v := range i {
-		total += v
-	}
-	return total
+func foo() {
+	fmt.Println("foo")
+}
+
+func bar() {
+	fmt.Println("bar")
 }
