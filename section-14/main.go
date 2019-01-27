0e5eb0c5a25889c78361190bf462408b87322777
Zun <zunsamuel97@gmail.com>
2019-01-26 21:41:41 -0500

    reciever methods

--- section-14/main.go
+++ section-14/main.go
@@ -2,15 +2,36 @@ package main
 
 import "fmt"
 
+type person struct {
+	first string
+	last  string
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func (s secretAgent) speak() {
+	fmt.Println(s.last+",", s.first, s.last)
+}
+
 func main() {
-	defer foo()
-	bar()
-}
-
-func foo() {
-	fmt.Println("foo")
-}
-
-func bar() {
-	fmt.Println("bar")
+	sa1 := secretAgent{
+		person: person{
+			first: "James",
+			last:  "Bond",
+		},
+		ltk: true,
+	}
+	sa2 := secretAgent{
+		person: person{
+			first: "Ms",
+			last:  "MP",
+		},
+		ltk: true,
+	}
+	fmt.Println(sa1)
+	sa1.speak()
+	sa2.speak()
 }
