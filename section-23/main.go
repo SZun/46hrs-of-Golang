8ac74b390a0c2fb8dffbfab33055c8847700d4d6
Zun <zunsamuel97@gmail.com>
2019-01-27 19:00:02 -0500

    hoe10-4

--- section-23/main.go
+++ section-23/main.go
@@ -3,24 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	c := gen()
-	recieve(c)
+	q := make(chan int)
+	c := gen(q)
+	recieve(c, q)
 	fmt.Println("about to exit")
 }
 
-func gen() <-chan int {
+func recieve(c, q <-chan int) {
+	for {
+		select {
+		case v := <-c:
+			fmt.Println(v)
+		case <-q:
+			return
+		}
+	}
+}
+
+func gen(q chan<- int) <-chan int {
 	c := make(chan int)
 	go func() {
 		for i := 0; i < 100; i++ {
 			c <- i
 		}
+		q <- 1
 		close(c)
 	}()
 	return c
-}
-
-func recieve(c <-chan int) {
-	for v := range c {
-		fmt.Println(v)
-	}
 }
