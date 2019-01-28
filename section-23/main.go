450be611f6f8327665345d4dc6d2f4adabd4a746
Zun <zunsamuel97@gmail.com>
2019-01-27 19:09:51 -0500

    hoe10-7

--- section-23/main.go
+++ section-23/main.go
@@ -4,14 +4,16 @@ import "fmt"
 
 func main() {
 	c := make(chan int)
-	go func() {
-		for i := 0; i < 100; i++ {
-			c <- i
-		}
-		close(c)
-	}()
-	for v := range c {
-		fmt.Println(v)
+
+	for j := 0; j < 10; j++ {
+		go func() {
+			for i := 0; i < 10; i++ {
+				c <- i
+			}
+		}()
+	}
+	for k := 0; k < 100; k++ {
+		fmt.Println(k, <-c)
 	}
 	fmt.Println("about to exit")
 }
